3e2cb3b2e7d21afabaea4a80289c3f3d37964960
chelsea2point0 <chelseajoyschaefer@gmail.com>
2015-11-12 18:06:33 -0500

    added github.js file and modified script.js to create custom service for http requests and response data

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-(function(){
+(function() {
 
 	var app = angular.module("githubViewer", []);
 
-	var MainController = function(
-		$scope, $http, $interval, 
-		$log, $anchorScroll, $location){
+	var MainController = function($scope, github, $interval, $log, $anchorScroll, $location){
 
-		var onUserComplete = function(response) {
-			$scope.user = response.data;
-			$http.get($scope.user.repos_url)
-				.then(onRepos, onError);
+		var onUserComplete = function(data) {
+			$scope.user = data;
+			github.getRepos($scope.user).then(onRepos, onError);
 		}
 
-		var onRepos = function(response){
-			$scope.repos = response.data;
+		var onRepos = function(data){
+			$scope.repos = data;
 			$location.hash("userDetails");
 			$anchorScroll();
 		}
@@ -36,8 +33,7 @@
 
 		$scope.search = function(username) {
 			$log.info("Searching for " + username);
-			$http.get("https://api.github.com/users/" + username)
-				.then(onUserComplete, onError);
+			github.getUser(username).then(onUserComplete, onError);
 			if (countdownInterval) {
 				$interval.cancel(countdownInterval);
 				$scope.countdown = null;
