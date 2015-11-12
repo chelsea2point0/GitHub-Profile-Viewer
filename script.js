8a3bdc2cfd7ebdeea998fc1f6b45c243d11aaf70
chelsea2point0 <chelseajoyschaefer@gmail.com>
2015-11-12 16:30:44 -0500

    added interval service function to generate countdown feature. If user does not search within 5 seconds of page load, it will automatically search for the default 'angular' username

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 	var app = angular.module("githubViewer", []);
 
-	var MainController = function($scope, $http){
+	var MainController = function($scope, $http, $interval){
 
 		var onUserComplete = function(response) {
 			$scope.user = response.data;
@@ -18,6 +18,17 @@
 			$scope.error = "Could not fetch the data.";
 		}
 
+		var decrementCountdown = function() {
+			$scope.countdown -= 1;
+			if ($scope.countdown < 1) {
+				$scope.search($scope.username);
+			}
+		}
+
+		var startCountdown = function() {
+			$interval(decrementCountdown, 1000, $scope.countdown);
+		}
+
 		$scope.search = function(username) {
 			$http.get("https://api.github.com/users/" + username)
 				.then(onUserComplete, onError);
@@ -26,6 +37,8 @@
 		$scope.username = "angular";
 		$scope.message = "Github Viewer";
 		$scope.repoSortOrder = "-stargazers_count";
+		$scope.countdown = 5;
+		startCountdown();
 	};
 
 	app.controller("MainController", MainController);
